11c43c241f458fc85437e603e5cd71584e11d8e8
IT User1 <dishendramishra@gmail.com>
2024-08-08 10:20:57 +0530

    QThreads implemented

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-import sys, os
+import sys, os, traceback
 
-from PySide6.QtCore import QSize, Qt
+from PySide6.QtCore import *
 from PySide6.QtWidgets import * #QApplication, QMainWindow, QPushButton, QFileDialog
 from PySide6.QtGui import *
 from pprint import pprint
 
 from modules import pdf_utils
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    pass
+
+elif sys.platform == "win32":
+    import ctypes
+    myappid = u'mycompany.myproduct.subproduct.version'  # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+elif sys.platform == "darwin":
+    pass
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
@@ -21,10 +32,10 @@ class MainWindow(QMainWindow):
         self.btn_select.clicked.connect(self.selectFiles)
         
         self.btn_merge = QPushButton("Merge Files")
-        self.btn_merge.clicked.connect(self.mergeFiles)
+        self.btn_merge.clicked.connect(self.start_merge_pdf_thread)
         
         self.btn_convert = QPushButton("Convert")
-        self.btn_convert.clicked.connect(self.convertFiles)
+        self.btn_convert.clicked.connect(self.convert_pdf_thread)
         
         self.txt_edit = QTextEdit()
         self.txt_edit.setReadOnly(True)
@@ -33,6 +44,9 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.txt_edit,1,0,1,2)
         self.layout.addWidget(self.btn_merge,2,0)
         self.layout.addWidget(self.btn_convert,2,1)
+
+        self.btn_convert.setDisabled(True)
+        self.btn_merge.setDisabled(True)
         
         widget = QWidget()
         widget.setLayout(self.layout)
@@ -49,6 +63,10 @@ class MainWindow(QMainWindow):
         self.txt_edit.textCursor().insertHtml("<font color='{}'>{}</font>{}".format(color,msg,end))
 
     def selectFiles(self):
+        self.clear()
+        self.btn_merge.setDisabled(True)
+        self.btn_convert.setDisabled(True)
+        
         filename = QFileDialog()
         filename.setFileMode(QFileDialog.ExistingFiles)
         names = filename.getOpenFileNames(self, "Select PDF Files", ".", "PDF (*.pdf)")
@@ -57,15 +75,37 @@ class MainWindow(QMainWindow):
         names.sort()
         self.log("<br>".join(names))
 
-    def mergeFiles(self):
-        output = pdf_utils.mergePDFs(self.list_selected_files, "merged.pdf")
-        self.log(f"Merged PDF created: {output}", color="blue")
+        if len(self.list_selected_files) > 0:
+            if len(self.list_selected_files) > 1:
+                self.btn_merge.setDisabled(False)
+            self.btn_convert.setDisabled(False)
 
-    def convertFiles(self):
-        pdf_utils.pdfToDocx(self.list_selected_files)
-        self.log("File(s) converted.", color="blue")
+    def start_merge_pdf_thread(self): 
+        self.btn_merge.setDisabled(True)
+        
+        if len(self.list_selected_files) < 2:
+            self.log("Please select atleast 2 files for merging!", "red")
+
+        else:    
+            self.thread = pdf_utils.mergePDFsThread(self.list_selected_files, "merged.pdf")
+            self.thread.log.connect(self.log)
+            self.thread.thread_finished.connect(self.done_merge_pdf_thread)
+            self.thread.start()
+    
+    def done_merge_pdf_thread(self):
+        self.btn_merge.setDisabled(False)
+        self.btn_convert.setDisabled(False)
+
+    def convert_pdf_thread(self):
+        self.thread = pdf_utils.pdfToDocxThread(self.list_selected_files)
+        self.thread.msg.connect(self.log)
+        self.thread.start()
+
+    def clear(self):
+        self.txt_edit.clear()
 
 app = QApplication(sys.argv)
+app.setStyle("Fusion")
 window = MainWindow()
 window.show()
 app.exec()
--- modules/pdf_utils.py
+++ modules/pdf_utils.py
@@ -1,30 +1,63 @@
 from PyPDF2 import PdfMerger
 from pdf2docx import Converter
 from pdf2docx import parse
-import glob
 import os
 import time
 
-def mergePDFs(list_pdfs, outname_filename):
-    merger = PdfMerger()
-    for pdf in list_pdfs:
-        merger.append(open(pdf, "rb"))
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
 
-    if os.path.isfile(outname_filename):
-        suffix = str(int(time.time()))
-        outname_filename = outname_filename.replace(".", "_"+suffix+".")
+class mergePDFsThread(QThread):
 
-    with open(outname_filename, "wb") as fout:
-        merger.write(fout)
+    log = Signal(str,str)
+    thread_finished = Signal(int)
 
-    return(outname_filename)
+    def __init__(self, list_pdfs, outname_filename):
+        super().__init__()
+        self.list_pdfs        = list_pdfs
+        self.outname_filename = outname_filename
+
+    def run(self):
+        merger = PdfMerger()
+
+        for pdf in self.list_pdfs:
+            merger.append(open(pdf, "rb"))
+
+        if os.path.isfile(self.outname_filename):
+            suffix = str(int(time.time()))
+            self.outname_filename = self.outname_filename.replace(".", "_"+suffix+".")
+
+        with open(self.outname_filename, "wb") as fout:
+            merger.write(fout)
+
+        self.log.emit("Merging Files..", "blue")
+        time.sleep(0.2)
+        self.log.emit(f"merged PDF: {self.outname_filename}", "blue")
+        print("mergePDFsThread finished!")
+        self.thread_finished.emit(1)
+
+    def stop(self):
+        self.is_running = False
+        self.terminate()
+
+class pdfToDocxThread(QThread):
+    
+    msg = Signal(str,str)
+
+    def __init__(self, list_pdfs):
+        super().__init__()
+        self.list_pdfs = list_pdfs
+    
+    def run(self):
+        if len(self.list_pdfs) > 0:
+            for i in  self.list_pdfs:
+                parse(i, i+".docx", start=0, end=None)
+        
+        self.msg.emit("Conversion Completed!","blue")
+            
 
 def pdfToDocx(list_pdfs):
     if len(list_pdfs) > 0:
         for i in  list_pdfs:
             parse(i, i+".docx", start=0, end=None)
-
-
-# if __name__ == "__main__":
-# mergePDFs(glob.glob("*.pdf"),"merged.pdf")
-# pdfToDocx("page2.pdf", "page2.docx")
